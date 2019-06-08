30bf691198882b022b25a25bfda5a689df859d09
Chris Corbett <cccorbett1915@gmail.com>
2019-06-08 13:08:25 -0400

    Caesar cipher encoding now working

--- src/com/chris/Caesar.java
+++ src/com/chris/Caesar.java
@@ -2,7 +2,14 @@ package com.chris;
 
 public class Caesar {
 	public String encode(int key, String input) {
-		
-		return "";
+		String code = "";
+		for (int i = 0; i < input.length(); i++) {
+			if (Character.isUpperCase(input.charAt(i))) {
+				code += (char)(((int)input.charAt(i) + key - 65) % 26 + 65);
+			} else {
+				code += (char)(((int)input.charAt(i) + key - 97) % 26 + 97);
+			}
+		}
+		return code;
 	}
 }
--- src/com/chris/Main.java
+++ src/com/chris/Main.java
@@ -6,37 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		WordArr word = new WordArr();
+		Caesar ca = new Caesar();
 		//System.out.println(word.words.length);
 
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Please enter the key: ");
+		/*System.out.print("Please enter the key: ");
 		String keyS = sc.nextLine();
 		int key = Integer.parseInt(keyS);
 		System.out.print("Please enter the string you would like to encode: ");
-		String str = sc.nextLine();
-		String code = "";
-		char[] strArr = str.toCharArray();
-		for (int i = 0; i < strArr.length; i++) {
-			char let = strArr[i];
-			if (Character.isUpperCase(let)) {
-				if (let + key > 90) {
-					let -= key;
-				} else {
-					let += (key - 65) % 26 + 65;
-				}
-				code += (char)let;
-			} else if (Character.isLowerCase(let)) {
-				if (let + key > 122) {
-					let += (key - 65) % 26 + 65;
-				} else {
-					let += (key - 65) % 26 + 65;
-				}
-				code += (char)let;
-			}
+		String str = sc.nextLine();*/
+
+		String st = "hello";
+		int key = 5;
+		for (int i = 1; i <= 26; i++) {
+			System.out.println(ca.encode(i, st));
 		}
 
-		System.out.println(code);
-		//System.out.println(str);
+
 
 	}
 
