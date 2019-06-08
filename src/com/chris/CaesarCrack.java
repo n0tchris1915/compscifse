52cd223425a178c5bef49b5ac38c08852f0c5b2c
Chris Corbett <cccorbett1915@gmail.com>
2019-06-08 13:20:50 -0400

    Started working on CaesarCrack.java

--- src/com/chris/CaesarCrack.java
+++ src/com/chris/CaesarCrack.java
@@ -1,4 +1,14 @@
 package com.chris;
 
 public class CaesarCrack {
+	public String crack(String input) {
+		WordArr word = new WordArr();
+
+		for (int i = 1; i < 26; i++) {
+			for (int j = 0; j < input.length(); j++) {
+
+			}
+		}
+		return "";
+	}
 }
